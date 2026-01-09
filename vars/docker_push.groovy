af5cd2b3ff97dfb66e6aa77ec190592dfb88fa14
Pavan kumar <pawankumar14662693@gmail.com>
2026-01-09 22:56:39 +0530

    Escape variables in docker login command

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,10 +4,10 @@ def call(String project, String imageTag, String dockerHubUser) {
         usernameVariable: 'DOCKER_HUB_USER',
         passwordVariable: 'DOCKER_HUB_PASS'
     )]) {
-        sh '''
-        echo "$DOCKER_HUB_PASS" | docker login -u "$DOCKER_HUB_USER" --password-stdin
+        sh """
+        echo "\$DOCKER_HUB_PASS" | docker login -u "\$DOCKER_HUB_USER" --password-stdin
         docker tag ${project}:${imageTag} ${dockerHubUser}/${project}:${imageTag}
         docker push ${dockerHubUser}/${project}:${imageTag}
-        '''
+        """
     }
 }
